bbc4dec495cfecd3295d4d79e5f561ef0e20ed27
sahana <shayaandoodekula950@gmail.com>
2025-12-20 00:04:17 -0500

    she lint my code

---
+++ eslint.config.cjs
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        ignores: [
+            "dist/**",
+            "node_modules/**",
+            "*.config.js",
+        ],
+    },
+
+    require("@eslint/js").configs.recommended,
+    ...require("typescript-eslint").configs.recommended,
+];
