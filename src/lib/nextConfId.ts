d7b07a2ce078639fbf85030c07cf0d1f970d345a
sahana <github@sahana.dev>
2025-12-27 00:56:55 -0500

    did you know i love my wife @astra ASAKDJSAKDJ I LOVE HER AAA <3 MWAH WAM HWAHJWIAJDKS ASDNASDNA EXPLODES AND DIES

--- src/lib/nextConfId.ts
+++ src/lib/nextConfId.ts
@@ -1,3 +1,12 @@
-export default function nextConfessionId() {
+import { db } from "@/index";
 
+export default function nextConfessionId() {
+	const result = db
+		.selectFrom("confessions")
+		.select(db.fn.max("id").as("maxId"))
+		.executeTakeFirstSync();
+	if (!result) {
+		return 1
+	}
+	return result
 }
