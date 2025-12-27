01e7d8f3e61069e68964b780a8e7486d87389d17
sahana <hana@girlin.tech>
2025-12-27 01:46:06 -0500

    she catch my error wen she

--- src/lib/stageConfess.ts
+++ src/lib/stageConfess.ts
@@ -1,7 +1,30 @@
 import crypto from "crypto";
+import * as s from '@/db/schema'
+import { db } from "@/db"
+import nextConfessionId from "@/lib/nextId";
 
-export default function stageConfession(message: string, slackId: string) {
-	const salt = crypto.randomBytes(16).toString("hex"); // stolen from ani
+export default async function stageConfession(message: string, slackId: string) {
+	try {
+		const salt = crypto.randomBytes(16).toString("hex"); // stolen from ani
 
-	const hash = crypto.scryptSync(Buffer.from(slackId), salt, 64).toString("hex");
+		const hash = crypto.scryptSync(Buffer.from(slackId), salt, 64).toString("hex");
+
+		const id = await nextConfessionId()
+
+		const confession: s.NewConfession = {
+			hash: hash,
+			salt: salt,
+			published: false,
+			content: message,
+			stagingTs: 0, // TODO: PUT A STANGIGN TS HERE
+			id: id
+		}
+
+		await db.insert(s.confessions).values(confession)
+
+		return true
+	} catch (err) {
+		console.log(err)
+		return false
+	}
 }
