e7f81df46b25bea07eefdfb2be46f838fb575b10
sahana <hana@girlin.tech>
2026-02-18 15:10:00 -0500

    lint is good lint is good

--- src/register/index.ts
+++ src/register/index.ts
@@ -1,8 +1,8 @@
 import type { Elysia } from "elysia";
-import command from "@/command"
-import events from "@/events"
+import command from "@/command";
+import events from "@/events";
 
-export default function register(app: Elysia){
-    app.get("/api/prox3", command)
-    app.get("/api/event", events)
+export default function register(app: Elysia) {
+	app.get("/api/prox3", command);
+	app.get("/api/event", events);
 }
