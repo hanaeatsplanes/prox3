be7ddac899e5b891b38ccffb3cb784aafdfb0cf8
sahana <github@sahana.dev>
2026-02-20 09:56:59 -0500

    gasd

--- src/events/index.ts
+++ src/events/index.ts
@@ -3,10 +3,18 @@ import { validateSlackRequest } from "@/utils";
 
 const app = new Elysia();
 
-app.post("/api/events", async (context: Context) => {
-    const { request, } = context;
-    if (!await validateSlackRequest(request)) {
+app.post("/api/events", async ({ request, status }: Context) => {
+    const rawBody = await validateSlackRequest(request)
+    if (!rawBody) {
+        status("Unauthorized")
+        return JSON.stringify({ status: "unauthorized" })
+    }
 
+    const body = JSON.parse(rawBody)
+
+    const { type } = body;
+    if (type === "url_verification") {
+        return body.challenge
     }
 });
 
