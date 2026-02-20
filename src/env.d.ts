fee45c75895761fceaceffe54f65c3e0d3648ed4
sahana <github@sahana.dev>
2026-02-20 10:00:01 -0500

    format

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 declare namespace NodeJS {
-	interface ProcessEnv {
-		SLACK_SIGNING_SECRET: string;
-		SLACK_CLIENT_ID: string;
-		SLACK_TOKEN: string;
-	}
+  interface ProcessEnv {
+    SLACK_SIGNING_SECRET: string
+    SLACK_CLIENT_ID: string
+    SLACK_TOKEN: string
+  }
 }
--- src/events/index.ts
+++ src/events/index.ts
@@ -1,21 +1,21 @@
-import { Elysia, type Context } from "elysia";
-import { validateSlackRequest } from "@/utils";
+import { type Context, Elysia } from "elysia"
+import { validateSlackRequest } from "@/utils"
 
-const app = new Elysia();
+const app = new Elysia()
 
 app.post("/api/events", async ({ request, status }: Context) => {
-    const rawBody = await validateSlackRequest(request)
-    if (!rawBody) {
-        status("Unauthorized")
-        return JSON.stringify({ status: "unauthorized" })
-    }
+  const rawBody = await validateSlackRequest(request)
+  if (!rawBody) {
+    status("Unauthorized")
+    return JSON.stringify({ status: "unauthorized" })
+  }
 
-    const body = JSON.parse(rawBody)
+  const body = JSON.parse(rawBody)
 
-    const { type } = body;
-    if (type === "url_verification") {
-        return body.challenge
-    }
-});
+  const { type } = body
+  if (type === "url_verification") {
+    return body.challenge
+  }
+})
 
-export default app;
+export default app
