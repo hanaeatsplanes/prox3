1c155eb8f0a08d452874baa0840ede6c5c250513
sahana <hana@girlin.tech>
2026-02-24 13:48:03 -0500

    gah

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,5 +6,11 @@ declare namespace NodeJS {
     CONFESSIONS: string;
     CONFESSIONS_LOG: string;
     CONFESSIONS_REVIEW: string;
+    POSTGRES_USER: string;
+    POSTGRES_PASSWORD: string;
+    POSTGRES_DB: string;
+    POSTGRES_PORT: number;
+    REDIS_PORT: number;
+    REDIS_PASSWORD: string;
   }
 }
