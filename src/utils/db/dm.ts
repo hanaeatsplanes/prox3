12ccf11d0fa39d06ba7c4d609a16376d83ffc0e8
sahana <github@sahana.dev>
2026-02-25 14:10:08 -0500

    how could a heart like yours, ever love a heart like mine

--- src/utils/db/dm.ts
+++ src/utils/db/dm.ts
@@ -1,11 +1,8 @@
 import { redis } from "bun";
 
-export function nextId() {
-  return 1; //todo: implement
-}
 export async function hasStaged(ts: string) {
   return !!(await redis.get(`dm-ts:${ts}`));
 }
 export async function setStaged(ts: string) {
-  return await redis.setex(`dm-ts:${ts}`, 30, "1");
+  return await redis.setex(`dm-ts:${ts}`, 0, "1");
 }
