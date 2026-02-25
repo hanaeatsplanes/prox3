af71040d42b636d7c8fa2fca9c9b82a0ef372b24
sahana <hana@girlin.tech>
2026-02-25 08:54:00 -0500

    ugahsd

---
+++ src/models/error.ts
@@ -0,0 +1,7 @@
+export class ErrorWithStatus extends Error {
+  public readonly statusCode: number;
+  constructor(message: string, statusCode = 500) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
