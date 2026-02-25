ca8f978981431e907ba9321a3868e001f396cf3d
sahana <hana@girlin.tech>
2026-02-25 13:59:48 -0500

    wwweeegit commit -m grr

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+import { redis } from "bun";
 import { Elysia } from "elysia";
 import command from "@/command";
 import events from "@/events";
 
+await redis.connect();
+
 const app = new Elysia();
 app.use(command);
 app.use(events);
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
