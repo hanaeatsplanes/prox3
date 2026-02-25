04f900b05a54ddaf59777eea774ba78cf3fafc94
sahana <github@sahana.dev>
2026-02-25 11:04:16 -0500

    update yo chat

--- src/utils/slack/client.ts
+++ src/utils/slack/client.ts
@@ -40,3 +40,32 @@ export async function postMessage(
   if (!data.ok) throw new ErrorWithStatus(`Slack API: ${data.error}`, 500);
   return data.ts;
 }
+
+export async function chatUpdate(
+  ts: string,
+  channel: string,
+  content: string | object[]
+) {
+  const isText = typeof content === "string";
+  const response = await fetch("https://slack.com/api/chat.update", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      ts,
+      channel,
+      ...(isText ? { text: content } : { blocks: content }),
+    }),
+  });
+  if (!response.ok) throw new Error(`Slack API error: ${response.status}`);
+
+  const data = (await response.json()) as {
+    ok: boolean;
+    error?: string;
+    ts: string;
+  };
+  if (!data.ok) throw new ErrorWithStatus(`Slack API: ${data.error}`, 500);
+  return data.ts;
+}
