260cde5f235cc839d5891ad3570d56d97f6261b0
sahana <hana@girlin.tech>
2026-02-26 10:37:11 -0500

    my database had a table

---
+++ sql/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE confessions (
+    id INTEGER PRIMARY KEY,
+    confession TEXT NOT NULL,
+    hash TEXT NOT NULL,
+    channel TEXT,
+    staging_ts TEXT UNIQUE,
+    state TEXT NOT NULL
+        CHECK ( state in ('approved',
+                          'rejected',
+                          'staged',
+                          'unstaged'
+                         ))
+)
