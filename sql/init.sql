ca133849165b6c260f5212700d32782157074ca3
sahana <hana@girlin.tech>
2026-03-06 08:54:03 -0500

    FAH

--- sql/init.sql
+++ sql/init.sql
@@ -20,12 +20,3 @@ CREATE TABLE IF NOT EXISTS confessions (
                          )),
     approval_ts TEXT UNIQUE
 );
-
-
-ALTER TABLE IF EXISTS confessions
-    ADD COLUMN IF NOT EXISTS confession TEXT NOT NULL DEFAULT '',
-    ADD COLUMN IF NOT EXISTS hash TEXT NOT NULL DEFAULT '',
-    ADD COLUMN IF NOT EXISTS channel TEXT,
-    ADD COLUMN IF NOT EXISTS staging_ts TEXT UNIQUE,
-    ADD COLUMN IF NOT EXISTS state TEXT NOT NULL DEFAULT 'unstaged',
-    ADD COLUMN IF NOT EXISTS approval_ts TEXT UNIQUE;
