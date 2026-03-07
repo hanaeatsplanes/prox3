5821d887861dae78c6c8e4ee9f58588ad7307988
sahana <hana@girlin.tech>
2026-03-06 19:33:29 -0500

    sadfsafsa

--- src/command/index.ts
+++ src/command/index.ts
@@ -1,13 +1,13 @@
 import { type Context, Elysia } from "elysia";
 import { Confession } from "@/models/confession.ts";
-import { chatPostMessage } from "@/utils/slack/client.ts";
 import { extractCommandBody } from "@/utils/slack/middleware.ts";
 
 export default new Elysia().post("/api/command", handler);
 
 async function handler({ request }: Context): Promise<void> {
-  const body = extractCommandBody(await request.text());
-  const confession = await Confession.create(body.text, body.user_id);
+  const { text, user_id } = extractCommandBody(await request.text());
+  // todo: check for revive
+  const confession = await Confession.create(text, user_id);
   await confession.stage();
   await confession.updateDB();
 }
