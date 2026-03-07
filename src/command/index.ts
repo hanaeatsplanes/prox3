35303d08211585767bb8aa990b6815564bcdd9d1
sahana <hana@girlin.tech>
2026-03-06 19:34:32 -0500

    asdfsjakdjklfjkla

--- src/command/index.ts
+++ src/command/index.ts
@@ -1,5 +1,6 @@
 import { type Context, Elysia } from "elysia";
 import { Confession } from "@/models/confession.ts";
+import { chatPostMessage } from "@/utils/slack/client.ts";
 import { extractCommandBody } from "@/utils/slack/middleware.ts";
 
 export default new Elysia().post("/api/command", handler);
@@ -10,4 +11,5 @@ async function handler({ request }: Context): Promise<void> {
   const confession = await Confession.create(text, user_id);
   await confession.stage();
   await confession.updateDB();
+  await chatPostMessage(user_id, `Staged as confession ${confession.id}`);
 }
