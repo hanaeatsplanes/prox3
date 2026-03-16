4372e7d08d8f0f85f8a32b88568535f16cc19e52
sahana <hana@girlin.tech>
2026-03-16 09:38:30 -0400

    grr

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ await redis.connect();
 export default new Elysia()
   .use(command)
   .use(events)
-  .listen(3000, () => {
+  .listen({ hostname: "0.0.0.0", port: 3000 }, () => {
     console.log("Listening on port 3000");
   });
