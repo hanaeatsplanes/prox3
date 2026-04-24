1e35054d8b9a60955434a16ef0f078519930c6a4
sahana <github@sahana.dev>
2026-04-24 13:25:10 -0400

    move

--- src/core/events/handlers/blockActionHandler.ts
+++ src/core/events/handlers/blockActionHandler.ts
@@ -1,8 +1,9 @@
+import { twModal } from "@/config/language";
 import { Confession } from "@/core/confession.ts";
 import type { BlockActionEvent } from "@/models/event.ts";
+import { cache, isCached, isUndoCached, undoCache } from "@/utils/db/cache.ts";
 import { getConfessionBy } from "@/utils/db/confession.ts";
-import { cache, isCached, isUndoCached, undoCache } from "@/utils/db/dm.ts";
-import { chatDelete, chatUpdate } from "@/utils/slack/client.ts";
+import { chatDelete, chatUpdate, viewsOpen } from "@/utils/slack/client.ts";
 
 export default async function (body: BlockActionEvent) {
 	const action = body.actions[0];
@@ -12,12 +13,12 @@ export default async function (body: BlockActionEvent) {
 	const { container } = body;
 	if (
 		(await isCached(container.message_ts)) &&
-		(action.action_id !== "undo" || (await isUndoCached(container.message_ts)))
+		(action.action_id !== "undo" || !(await isUndoCached(container.message_ts)))
 	) {
 		return;
 	}
 	void cache(container.message_ts);
-	if (action.action_id !== "undo") {
+	if (action.action_id === "undo") {
 		void undoCache(container.message_ts);
 	}
 	switch (action.action_id) {
@@ -72,10 +73,11 @@ export default async function (body: BlockActionEvent) {
 			await confession.undo(body.user.id);
 			break;
 		}
-		case "approve:tw":
-			throw new Error(
-				`[button] action "${action.action_id}" is not yet implemented`
-			);
+		case "approve:tw": {
+			const ts = container.message_ts;
+			await viewsOpen(body.trigger_id, twModal(ts));
+			break;
+		}
 		default: {
 			throw new Error(`[button] unhandled action: ${action.action_id}`);
 		}
