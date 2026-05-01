5d78bab62ccf8fe0a747cb5185455dd432b93890
sahana <github@sahana.dev>
2026-05-01 11:58:12 -0400

    dkslfnsadlj"

--- src/core/events/handlers/messageActionHandler.ts
+++ src/core/events/handlers/messageActionHandler.ts
@@ -1,9 +1,14 @@
+import { confessionChannel } from "@/models/channels.ts";
 import type { MessageActionEvent } from "@/models/event.ts";
 
 export default async function messageActionHandler({
 	response_url,
 	message,
+	channel,
 }: MessageActionEvent) {
+	if (!(channel.id in confessionChannel)) {
+		return;
+	}
 	if (message.bot_profile?.id !== process.env.SLACK_BOT_ID) {
 		await fetch(response_url, {
 			body: JSON.stringify({
